7cb754de3608929d03c5b4581420052cb5674c53
NguyenKhoa311 <160843987+NguyenKhoa311@users.noreply.github.com>
2024-03-07 11:04:01 +0700

    Create classes and objects.cpp

---
+++ LTNC_6/classes and objects.cpp
@@ -0,0 +1,24 @@
+
+
+// Write your Student class here
+
+class Student{
+    private:
+       vector<int> scores;
+    public:
+        void input(){
+            for (int i = 0; i < 5; i++){
+               int x;
+               cin >> x;
+               scores.push_back(x);
+            }
+        }
+        int calculateTotalScore(){
+            int sum = 0;
+            for (int i = 0; i < 5; i++){
+                sum += scores[i];
+            }
+            return sum;
+        }
+};
+
