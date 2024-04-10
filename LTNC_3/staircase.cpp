d10af1de0fccf59cbc34b2b07d647dc886b2052b
Khoa Nguyen <160843987+NguyenKhoa311@users.noreply.github.com>
2024-04-10 21:31:11 +0700

    Optimize

--- LTNC_3/staircase.cpp
+++ LTNC_3/staircase.cpp
@@ -12,25 +12,15 @@ string rtrim(const string &);
  */
 
 void staircase(int n) {
-    int m = n - 1;
-    int p = 1;
-    for(int i = 0; i < n; i++)
-    {
-    
-        for(int j = 0; j < m; j++)
-        {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n - i; ++j) {
             cout << " ";
         }
-        for(int k = 0; k < p; k++)
-        {
+        for (int k = 1; k <= i; ++k) {
             cout << "#";
         }
-        m--;
-        p++;
         cout << endl;
     }
-    
-
 }
 
 int main()
